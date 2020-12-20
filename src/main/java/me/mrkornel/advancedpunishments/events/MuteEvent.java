9e9330d37aac7dcc89c4a28a45d99d890c979263
Kornél <kornel842@gmail.pog>
2020-12-20 13:00:58 +0100

    Ban & Mute Time Handling

--- src/main/java/me/mrkornel/advancedpunishments/events/MuteEvent.java
+++ src/main/java/me/mrkornel/advancedpunishments/events/MuteEvent.java
@@ -7,6 +7,11 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
+
 public class MuteEvent implements Listener {
 
     private AdvancedPunishments plugin;
@@ -20,8 +25,24 @@ public class MuteEvent implements Listener {
     public void onPlayerChatEvent(AsyncPlayerChatEvent e) {
         Player player = e.getPlayer();
         if(plugin.getConfig().get("MuteList").equals(player.getDisplayName())) {
-            e.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "AdvancedPunishments " + ChatColor.GRAY + "» " + plugin.getConfig().get("MutedMessage") + " " + ChatColor.RED + "Reason: " + ChatColor.GRAY + plugin.getConfig().get(player.getUniqueId() + " MuteReason") + " " + ChatColor.RED + "Until: " + ChatColor.GRAY + plugin.getConfig().get(player.getUniqueId() + " MuteTime"));
+            Date currentDate = new Date();
+            Calendar MuteT = Calendar.getInstance();
+            MuteT.setTime(currentDate);
+            Date date = MuteT.getTime();
+
+
+            SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+            String date1 = format1.format(date);
+
+            int CurrentDate1 = Integer.parseInt(date1);
+            UUID playerID = player.getUniqueId();
+            int MuteTime = Integer.parseInt(player.getUniqueId().toString() + " MuteTime");
+            if(MuteTime > CurrentDate1) {
+                e.setCancelled(true);
+                player.sendMessage(ChatColor.RED + "AdvancedPunishments " + ChatColor.GRAY + "» " + plugin.getConfig().get("MutedMessage") + " " + ChatColor.RED + "Reason: " + ChatColor.GRAY + plugin.getConfig().get(player.getUniqueId() + " MuteReason") + " " + ChatColor.RED + "Until: " + ChatColor.GRAY + plugin.getConfig().get(player.getUniqueId() + " MuteTime"));
+            } else {
+                e.setCancelled(false);
+            }
         }
     }
 
--- src/main/java/me/mrkornel/advancedpunishments/events/PlayerLoginEvent.java
+++ src/main/java/me/mrkornel/advancedpunishments/events/PlayerLoginEvent.java
@@ -11,6 +11,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.UUID;
 
 import static java.lang.Integer.*;
 
@@ -26,7 +27,26 @@ public class PlayerLoginEvent implements Listener {
     public void onPlayerJoinEvent(org.bukkit.event.player.PlayerJoinEvent e) {
         Player player = e.getPlayer();
         if(player.getDisplayName().equals(plugin.getConfig().get("BanList"))) {
-            player.kickPlayer(ChatColor.RED + "AdvancedPunishments " + ChatColor.GRAY + "» " + (String) plugin.getConfig().get("DefaultBanMessage") + ChatColor.RED + " Reason: " + ChatColor.GRAY + plugin.getConfig().get("Reason") + ChatColor.RED + " Until: " + ChatColor.GRAY + plugin.getConfig().get("Time"));
+
+            Date currentDate = new Date();
+            Calendar BanTime = Calendar.getInstance();
+            BanTime.setTime(currentDate);
+            Date date = BanTime.getTime();
+
+
+            SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+            String date1 = format1.format(date);
+
+            int CurrentDate1 = Integer.parseInt(date1);
+            UUID playerID = player.getUniqueId();
+            int BanT = Integer.parseInt(player.getUniqueId().toString() + " BanTime");
+
+            if(BanT > CurrentDate1) {
+                player.kickPlayer(ChatColor.RED + "AdvancedPunishments " + ChatColor.GRAY + "» " + (String) plugin.getConfig().get("DefaultBanMessage") + ChatColor.RED + " Reason: " + ChatColor.GRAY + plugin.getConfig().get("Reason") + ChatColor.RED + " Until: " + ChatColor.GRAY + plugin.getConfig().get("Time"));
+            } else {
+
+            }
+
         }
     }
 }
